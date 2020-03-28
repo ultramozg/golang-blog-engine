7effe3988634dacd1bf7bcd3a35e328637fba5fe
ultramozg <d.candu@hotmail.com>
2020-03-28 21:07:36 +0200

    add gitCommit version print

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package main
 
-import "github.com/ultramozg/golang-blog-engine/app"
+import (
+	"flag"
+	"log"
+
+	"github.com/ultramozg/golang-blog-engine/app"
+)
+
+var gitCommit string
+
+func printVersion() {
+	log.Printf("Current build version: %s", gitCommit)
+}
 
 func main() {
+	versionFlag := flag.Bool("v", false, "Print the current version and exit")
+	flag.Parse()
+
+	if *versionFlag {
+		printVersion()
+		return
+	}
+
 	conf := app.NewConfig()
 	a := app.NewApp()
 	a.Initialize(conf)
