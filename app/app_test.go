b80c576097e85c605b455c5ca4504397c5aeb9ed
ultramozg <d.candu@hotmail.com>
2020-03-30 20:11:57 +0300

    Added first unit test

--- app/app_test.go
+++ app/app_test.go
@@ -6,13 +6,14 @@ import (
 	"testing"
 )
 
-func TestRoot(t *testing.T) {
+func Testroot(t *testing.T) {
+	a := NewApp()
 	req, err := http.NewRequest(http.MethodGet, "/", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(root)
+	handler := http.HandlerFunc(a.root)
 	handler.ServeHTTP(rr, req)
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("Root handler returned wrong status code: got %v want %v", status, http.StatusOK)
