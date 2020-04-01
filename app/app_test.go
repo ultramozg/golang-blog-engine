bcb5f077d69b17c2c271afec1d1f36da70233e6a
ultramozg <d.candu@hotmail.com>
2020-04-01 21:29:58 +0300

    update Testroot function

--- app/app_test.go
+++ app/app_test.go
@@ -3,6 +3,7 @@ package app
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -17,5 +18,9 @@ func Testroot(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("Root handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+	expected := `<p>Powered by Golang net/http package</p>`
+	if !strings.Contains(rr.Body.String(), expected) {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
 }
